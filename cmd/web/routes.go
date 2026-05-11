f8b2df6fc48152cd74b0872536ea2cb947eabf8e
Nikola Golijanin <golijanin.nikola@outlook.com>
2026-05-11 19:26:53 +0200

    lets-go: chapter 6

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
@@ -10,5 +10,6 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-	return mux
+
+	return app.recoverPanic(app.logRequest(commonHeaders(mux)))
 }
