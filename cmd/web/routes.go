936b53ede354b63b87665ae428bf04398e33434f
Nikola Golijanin <nikola.golijanin@blocknox.de>
2026-05-13 13:49:35 +0200

    lets-go: chapter 11

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,31 +1,27 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	// unprotected
-	mux.Handle("GET /{$}", app.sessionManager.LoadAndSave(preventCSRF(http.HandlerFunc(app.home))))
-	mux.Handle("GET /snippet/view/{id}", app.sessionManager.LoadAndSave(preventCSRF(http.HandlerFunc(app.snippetView))))
-	mux.Handle("GET /user/signup", app.sessionManager.LoadAndSave(preventCSRF(http.HandlerFunc(app.userSignup))))
-	mux.Handle("POST /user/signup", app.sessionManager.LoadAndSave(preventCSRF(http.HandlerFunc(app.userSignupPost))))
-	mux.Handle("GET /user/login", app.sessionManager.LoadAndSave(preventCSRF(http.HandlerFunc(app.userLogin))))
-	mux.Handle("POST /user/login", app.sessionManager.LoadAndSave(preventCSRF(http.HandlerFunc(app.userLoginPost))))
-
-	// protected
-	mux.Handle("GET /snippet/create",
-		app.sessionManager.LoadAndSave(
-			app.requireAuthentication(http.HandlerFunc(app.snippetCreate))))
-
-	mux.Handle("POST /snippet/create",
-		app.sessionManager.LoadAndSave(
-			app.requireAuthentication(http.HandlerFunc(app.snippetCreatePost))))
-
-	mux.Handle("POST /user/logout",
-		app.sessionManager.LoadAndSave(app.requireAuthentication(http.HandlerFunc(app.userLogoutPost))))
-
-	return app.recoverPanic(app.logRequest(commonHeaders(mux)))
+	dynamic := alice.New(app.sessionManager.LoadAndSave, preventCSRF, app.authenticate)
+	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
+	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
+	mux.Handle("GET /user/signup", dynamic.ThenFunc(app.userSignup))
+	mux.Handle("POST /user/signup", dynamic.ThenFunc(app.userSignupPost))
+	mux.Handle("GET /user/login", dynamic.ThenFunc(app.userLogin))
+	mux.Handle("POST /user/login", dynamic.ThenFunc(app.userLoginPost))
+	protected := dynamic.Append(app.requireAuthentication)
+	mux.Handle("GET /snippet/create", protected.ThenFunc(app.snippetCreate))
+	mux.Handle("POST /snippet/create", protected.ThenFunc(app.snippetCreatePost))
+	mux.Handle("POST /user/logout", protected.ThenFunc(app.userLogoutPost))
+	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+	return standard.Then(mux)
 }
--- go.mod
+++ go.mod
@@ -12,5 +12,6 @@ require (
 
 require (
 	filippo.io/edwards25519 v1.2.0 // indirect
+	github.com/justinas/alice v1.2.0 // indirect
 	github.com/justinas/nosurf v1.2.0 // indirect
 )
